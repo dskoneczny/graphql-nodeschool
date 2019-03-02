c4bc8b22decd8afc65ca09518b3f7c3ba299e6d2
Damian Skoneczny <damian.skoneczny@htdevelopers.com>
2019-03-02 12:25:37 +0100

    Database url

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const { GraphQLServer } = require('graphql-yoga')
+const { Client } = require('pg')
+
+// DATABASE CONNECTION
+// https://bit.ly/2SCIst1
+const DATABASE_URL = "postgres://ptzlbdyjlabixo:04a27a0b7e06081a397d2d49776427af9804b66a1870ffbf19d13668736a5b80@ec2-46-137-121-216.eu-west-1.compute.amazonaws.com:5432/d2rd2bavu9mj8a?ssl=true"
+const client = new Client({ connectionString: DATABASE_URL})
 
 // QUERIES
 const { hello, helloWithName } = require('./src/resolvers/Query/HelloQuery')
