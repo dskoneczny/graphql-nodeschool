f81c354d33ffbc929caf253389add2218658a105
Damian Skoneczny <damian.skoneczny@htdevelopers.com>
2019-03-02 12:55:06 +0100

    getOrders query

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ const { Client } = require('pg')
 
 // DATABASE CONNECTION
 // https://bit.ly/2SCIst1
-const DATABASE_URL = "postgres://ptzlbdyjlabixo:04a27a0b7e06081a397d2d49776427af9804b66a1870ffbf19d13668736a5b80@ec2-46-137-121-216.eu-west-1.compute.amazonaws.com:5432/d2rd2bavu9mj8a?ssl=true"
+// https://pastebin.com/CwCvmgk1
+// const DATABASE_URL = "postgres://ptzlbdyjlabixo:04a27a0b7e06081a397d2d49776427af9804b66a1870ffbf19d13668736a5b80@ec2-46-137-121-216.eu-west-1.compute.amazonaws.com:5432/d2rd2bavu9mj8a?ssl=true"
+const DATABASE_URL = "postgres://ckwpgabmxyxkit:99dc71fc5776ce4577b39eeac34c1b1d9966766db90dd22dc6b907371db437ee@ec2-46-137-170-51.eu-west-1.compute.amazonaws.com:5432/df5rbug20emf0f?ssl=true"
 const client = new Client({ connectionString: DATABASE_URL })
 client.connect()
 
 // QUERIES
 const { hello, helloWithName } = require('./src/resolvers/Query/HelloQuery')
 const { getUsers, getUser } = require('./src/resolvers/Query/UsersQuery')
-
+const { getOrders } = require('./src/resolvers/Query/OrdersQuery')
 // MUTATIONS
 const { addUser } = require('./src/resolvers/Mutation/UsersMutation')
 
@@ -21,7 +23,8 @@ const Query = {
   hello,
   helloWithName,
   getUsers,
-  getUser
+  getUser,
+  getOrders
 }
 
 const Mutation = {
