4f340708e27f72d479e98689956afe2dcc5063e4
Damian Skoneczny <damian.skoneczny@htdevelopers.com>
2019-03-02 11:06:19 +0100

    Nodemon + helloWithName

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 const { GraphQLServer } = require('graphql-yoga')
-
 // https://github.com/dskoneczny/graphql-nodeschool
-
 const typeDefs = `
   type Query {
     hello: String!
+    helloWithName(name: String!): String!
   }
 `
-
 const resolvers = {
   Query: {
-    hello: () => 'Hello World!'
+    hello: () => 'Hello World!',
+    helloWithName: (parent, params) => `Hello ${params.name}`
   }
 }
-
 const server = new GraphQLServer({
   typeDefs,
   resolvers
