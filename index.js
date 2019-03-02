4e510795b4b4557af84da29ae60b3d3fcc958515
Damian Skoneczny <damian.skoneczny@htdevelopers.com>
2019-03-02 11:17:51 +0100

    Cleanup

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const { GraphQLServer } = require('graphql-yoga')
+const { hello, helloWithName } = require('./src/resolvers/Query/HelloQuery')
 // https://github.com/dskoneczny/graphql-nodeschool
 
+const Query = {
+  hello,
+  helloWithName
+}
+
 const resolvers = {
-  Query: {
-    hello: () => 'Hello World!',
-    helloWithName: (parent, params) => `Hello ${params.name}`
-  }
+  Query
 }
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
